61a882caff65560cb7a59885559d350f9dd75551
Riki Konikoff <riki.konikoff@gmail.com>
2017-01-18 11:26:48 -0500

    added to dishes controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,10 +2,34 @@ class DishesController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
 
   def index
+    @dishes = Dish.all
   end
 
   def show
+    @dish = Dish.find(params[:id])
   end
 
-  
+  def new
+    @dish = Dish.new
+    @creator = User.find(params[:creator_id])
+  end
+
+  def create
+    @dish = Dish.new(dish_params)
+    @creator = User.find(params[:creator_id])
+    @dish.creator = @creator
+    if @dish.save
+      flash[:notice] = "Dish added successfully"
+      redirect_to @dish
+    else
+      flash[:notice] = @dish.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :description)
+  end
 end
