78eba2f9537a9d5d4c4dd176e5d575dda7150ae5
Riki Konikoff <riki.konikoff@gmail.com>
2017-01-26 10:41:33 -0500

    live updates

--- app/controllers/api/v1/votes_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::VotesController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  
-  def index
-    @votes = Votes.where(review_id: params([:review_id]))
-    render json: @votes
-  end
-end
